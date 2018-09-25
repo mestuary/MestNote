090763d7f2268aae1a9cb0ccad6dddb43c5279aa
ChenWei <875266382@qq.com>
2018-09-25 21:55:17 +0800

    add build
    build exe

---
+++ build.js
@@ -0,0 +1,10 @@
+var electronInstaller = require('electron-winstaller');
+
+resultPromise = electronInstaller.createWindowsInstaller({
+  appDirectory: './package/MestNote',
+  outputDirectory: '/build/MestNote',
+  authors: 'Mestuary',
+  exe: 'MestNote.exe'
+});
+
+resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
